e92ef35b8f7335ed7f5f51206ed5026caaaab640
Hirano <hnaoya1120@gmail.com>
2023-06-18 11:41:22 +0900

    タブでの表示切り替え

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,192 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { USER_LIST } from "./UserList";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { USER_LIST_TYPE } from "./UserListType";
 
 function App() {
-    const USER_LIST = [
-        {
-            id: 1,
-            name: "鈴木太郎",
-            role: "student",
-            email: "test1@happiness.com",
-            age: 26,
-            postCode: "100-0003",
-            phone: "0120000001",
-            hobbies: ["旅行", "食べ歩き", "サーフィン"],
-            url: "https://aaa.com",
-            studyMinutes: 3000,
-            taskCode: 101,
-            studyLangs: ["Rails", "Javascript"],
-            score: 68,
-        },
-        {
-            id: 2,
-            name: "鈴木二郎",
-            role: "mentor",
-            email: "test2@happiness.com",
-            age: 31,
-            postCode: "100-0005",
-            phone: "0120000002",
-            hobbies: ["サッカー", "ランニング", "筋トレ"],
-            url: "https://bbb.com",
-            experienceDays: 1850,
-            useLangs: ["Next.js", "GoLang"],
-            availableStartCode: 201,
-            availableEndCode: 302,
-        },
-        {
-            id: 3,
-            name: "鈴木三郎",
-            role: "student",
-            email: "test3@happiness.com",
-            age: 23,
-            postCode: "300-0332",
-            phone: "0120000003",
-            hobbies: ["アニメ", "ゲーム", "旅行"],
-            url: "https://ccc.com",
-            studyMinutes: 125000,
-            taskCode: 204,
-            studyLangs: ["Rails", "Next.js"],
-            score: 90,
-        },
-        {
-            id: 4,
-            name: "鈴木四郎",
-            role: "mentor",
-            email: "test4@happiness.com",
-            age: 31,
-            postCode: "100-0005",
-            phone: "0120000004",
-            hobbies: ["食べ歩き", "ランニング", "旅行"],
-            url: "https://ddd.com",
-            experienceDays: 260,
-            useLangs: ["PHP", "Javascript"],
-            availableStartCode: 103,
-            availableEndCode: 408,
-        },
-        {
-            id: 5,
-            name: "鈴木五郎",
-            role: "student",
-            email: "test5@happiness.com",
-            age: 22,
-            postCode: "300-0005",
-            phone: "0120000005",
-            hobbies: ["筋トレ", "ランニング"],
-            url: "https://eee.com",
-            studyMinutes: 47800,
-            taskCode: 305,
-            studyLangs: ["Next.js", "Rails"],
-            score: 84,
-        },
-        {
-            id: 6,
-            name: "鈴木六郎",
-            role: "mentor",
-            email: "test6@happiness.com",
-            age: 28,
-            postCode: "100-0007",
-            phone: "0120000006",
-            hobbies: ["ゲーム", "サッカー"],
-            url: "https://fff.com",
-            experienceDays: 260,
-            useLangs: ["PHP", "Javascript"],
-            availableStartCode: 101,
-            availableEndCode: 302,
-        },
-        {
-            id: 7,
-            name: "鈴木七郎",
-            role: "student",
-            email: "test7@happiness.com",
-            age: 24,
-            postCode: "300-0008",
-            phone: "0120000007",
-            hobbies: ["筋トレ", "ダーツ"],
-            url: "https://ggg.com",
-            studyMinutes: 26900,
-            taskCode: 401,
-            studyLangs: ["PHP", "Rails"],
-            score: 73,
-        },
-        {
-            id: 8,
-            name: "鈴木八郎",
-            role: "mentor",
-            email: "test8@happiness.com",
-            age: 33,
-            postCode: "100-0009",
-            phone: "0120000008",
-            hobbies: ["ランニング", "旅行"],
-            url: "https://hhh.com",
-            experienceDays: 6000,
-            useLangs: ["Golang", "Rails"],
-            availableStartCode: 301,
-            availableEndCode: 505,
-        },
-    ];
+    const [selectedTabNumber, setSelectedTabNumber] = useState(0);
+    const [userList, setUserList] = useState<USER_LIST_TYPE[]>(USER_LIST);
     console.log(USER_LIST);
-    return <div className="App">テスト</div>;
+
+    const handleSelect = (index: any, last: any) => {
+        console.log("Selected tab: " + index + ", Last tab: " + last);
+    };
+
+    const changeSelectedIndex = (index: number) => {
+        setSelectedTabNumber(index);
+    };
+
+    useEffect(() => {
+        setUserList(USER_LIST);
+    }, [userList]);
+    return (
+        <Tabs selectedIndex={selectedTabNumber} onSelect={handleSelect}>
+            <TabList>
+                <Tab onClick={() => changeSelectedIndex(0)}>全員</Tab>
+                <Tab onClick={() => changeSelectedIndex(1)}>生徒</Tab>
+                <Tab onClick={() => changeSelectedIndex(2)}>メンター</Tab>
+            </TabList>
+
+            <TabPanel>
+                <h2>全員表示</h2>
+                <div>
+                    <table border={1} width="500" cellPadding="0">
+                        <thead>
+                            <tr>
+                                <th>名前</th>
+                                <th>ロール</th>
+                                <th>メールアドレス</th>
+                                <th>年齢</th>
+                                <th>郵便番号</th>
+                                <th>電話番号</th>
+                                <th>趣味</th>
+                                <th>URL</th>
+                                <th>勉強時間</th>
+                                <th>課題番号</th>
+                                <th>勉強中の言語</th>
+                                <th>パピネススコア</th>
+                                <th>実務経験月数</th>
+                                <th>現場で使っている言語</th>
+                                <th>担当できる課題番号初め</th>
+                                <th>担当できる課題番号終わり</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userList.map((userInfo, index) => (
+                                <tr key={index}>
+                                    <td>{userInfo.name}</td>
+                                    <td>{userInfo.role}</td>
+                                    <td>{userInfo.email}</td>
+                                    <td>{userInfo.age}</td>
+                                    <td>{userInfo.postCode}</td>
+                                    <td>{userInfo.phone}</td>
+                                    <td>{userInfo.hobbies}</td>
+                                    <td>{userInfo.url}</td>
+                                    <td>{userInfo.studyMinutes}</td>
+                                    <td>{userInfo.taskCode}</td>
+                                    <td>{userInfo.studyLangs}</td>
+                                    <td>{userInfo.score}</td>
+                                    <td>{userInfo.experienceDays}</td>
+                                    <td>{userInfo.useLangs}</td>
+                                    <td>{userInfo.availableStartCode}</td>
+                                    <td>{userInfo.availableEndCode}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </TabPanel>
+            <TabPanel>
+                <h2>生徒のみ表示</h2>
+                <div>
+                    <table border={1} width="500" cellPadding="0">
+                        <thead>
+                            <tr>
+                                <th>名前</th>
+                                <th>ロール</th>
+                                <th>メールアドレス</th>
+                                <th>年齢</th>
+                                <th>郵便番号</th>
+                                <th>電話番号</th>
+                                <th>趣味</th>
+                                <th>URL</th>
+                                <th>勉強時間</th>
+                                <th>課題番号</th>
+                                <th>勉強中の言語</th>
+                                <th>パピネススコア</th>
+                                <th>実務経験月数</th>
+                                <th>現場で使っている言語</th>
+                                <th>担当できる課題番号初め</th>
+                                <th>担当できる課題番号終わり</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userList.map(
+                                (userInfo, index) =>
+                                    userInfo.role === "student" && (
+                                        <tr key={index}>
+                                            <td>{userInfo.name}</td>
+                                            <td>{userInfo.role}</td>
+                                            <td>{userInfo.email}</td>
+                                            <td>{userInfo.age}</td>
+                                            <td>{userInfo.postCode}</td>
+                                            <td>{userInfo.phone}</td>
+                                            <td>{userInfo.hobbies}</td>
+                                            <td>{userInfo.url}</td>
+                                            <td>{userInfo.studyMinutes}</td>
+                                            <td>{userInfo.taskCode}</td>
+                                            <td>{userInfo.studyLangs}</td>
+                                            <td>{userInfo.score}</td>
+                                            <td>{userInfo.experienceDays}</td>
+                                            <td>{userInfo.useLangs}</td>
+                                            <td>
+                                                {userInfo.availableStartCode}
+                                            </td>
+                                            <td>{userInfo.availableEndCode}</td>
+                                        </tr>
+                                    )
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </TabPanel>
+            <TabPanel>
+                <h2>メンターのみ表示</h2>
+                <div>
+                    <table border={1} width="500" cellPadding="0">
+                        <thead>
+                            <tr>
+                                <th>名前</th>
+                                <th>ロール</th>
+                                <th>メールアドレス</th>
+                                <th>年齢</th>
+                                <th>郵便番号</th>
+                                <th>電話番号</th>
+                                <th>趣味</th>
+                                <th>URL</th>
+                                <th>勉強時間</th>
+                                <th>課題番号</th>
+                                <th>勉強中の言語</th>
+                                <th>パピネススコア</th>
+                                <th>実務経験月数</th>
+                                <th>現場で使っている言語</th>
+                                <th>担当できる課題番号初め</th>
+                                <th>担当できる課題番号終わり</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userList.map(
+                                (userInfo, index) =>
+                                    userInfo.role === "mentor" && (
+                                        <tr key={index}>
+                                            <td>{userInfo.name}</td>
+                                            <td>{userInfo.role}</td>
+                                            <td>{userInfo.email}</td>
+                                            <td>{userInfo.age}</td>
+                                            <td>{userInfo.postCode}</td>
+                                            <td>{userInfo.phone}</td>
+                                            <td>{userInfo.hobbies}</td>
+                                            <td>{userInfo.url}</td>
+                                            <td>{userInfo.studyMinutes}</td>
+                                            <td>{userInfo.taskCode}</td>
+                                            <td>{userInfo.studyLangs}</td>
+                                            <td>{userInfo.score}</td>
+                                            <td>{userInfo.experienceDays}</td>
+                                            <td>{userInfo.useLangs}</td>
+                                            <td>
+                                                {userInfo.availableStartCode}
+                                            </td>
+                                            <td>{userInfo.availableEndCode}</td>
+                                        </tr>
+                                    )
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </TabPanel>
+        </Tabs>
+    );
 }
 
 export default App;
